281397c2c97673e09f06193573165ad8467e9710
Enrique <enrique.taleno16055@est.edu.ni>
2023-06-05 07:36:03 -0600

    primer commit

---
+++ 3.cpp
@@ -0,0 +1,11 @@
+/* Ejercicio: Palíndromo
+ Descripción: Escribe un programa que verifique si una palabra ingresada por el usuario es un
+ palíndromo (se lee igual de adelante hacia atrás que de atrás hacia adelante).
+ Requisitos:
+ - Utilizar un arreglo para almacenar la palabra.
+ - Utilizar ciclos y bifurcaciones para comparar los caracteres de la palabra.
+ - Utilizar una función para realizar la verificación y devolver un resultado booleano.
+*/
+
+#include<iostream>
+using namespace std;
---
+++ 4.cpp
@@ -0,0 +1,11 @@
+/*Ejercicio: Números primos
+ Descripción: Escribe un programa que reciba como entrada un número entero y determine si es
+ primo o no.
+ Requisitos:
+ - Utilizar una función para verificar si un número es primo.
+ - Utilizar una bifurcación para manejar el caso de un número primo o no primo.
+ - Utilizar un ciclo para comprobar si el número es divisible por otros números.
+*/
+
+#include<iostream>
+using namespace std;
